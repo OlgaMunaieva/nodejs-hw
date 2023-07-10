3198e50e22b6a1a911f635b2218e1e175c34098b
OlgaMunaieva <asyssync@gmail.com>
2023-07-10 23:43:24 +0300

    added avatar format

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
+
 const { User } = require("../../models/user");
 
 const avatarsDir = path.join("public", "avatars");
@@ -10,6 +12,13 @@ const updateAvatar = async (req, res, next) => {
   const filename = `${_id}${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
   await fs.rename(tempUpload, resultUpload);
+  await Jimp.read(resultUpload)
+    .then((image) => {
+      return image.cover(250, 250).write(resultUpload);
+    })
+    .catch((error) => {
+      throw error;
+    });
   const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
   res.json({
